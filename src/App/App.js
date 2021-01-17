9f3289f1158b1a1b5318bee19213ed7924be7ac1
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-17 23:57:26 +0100

    first commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import { fn, cu } from '../functions'
+import { cu } from '../functions'
 
 export default class App extends Component {
 

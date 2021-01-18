026ce410dc5f9c51fcc66759e55cd1c4fe47bf47
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-18 15:06:24 +0100

    first commit

--- src/App/App.js
+++ src/App/App.js
@@ -36,7 +36,7 @@ export default class App extends Component {
   commentFilter = (arr,ind) => {
     if(ind === arr.id){
       return(
-        <div className='w3-padding w3-small w3-margin-top w3-card w3-round-xlarge' style={{display:'block', maxWidth: '100%'}}>
+        <div className='w3-padding w3-small w3-margin-top w3-card w3-round-xlarge  w3-mobile' style={{display:'block', maxWidth: '100%'}}>
           <span >{arr.comment}</span>
         </div>
       )
@@ -52,7 +52,7 @@ export default class App extends Component {
           <div className='w3-col m3 l3 w3-hide-small'><br /></div>
           <div className='w3-col s12 m6 l6'>
             <form className=''>
-              <input type='search' placeholder='Search' id='search' className='w3-input w3-border w3-round w3-margin-top search' />
+              <input type='search' placeholder='Search' id='search' className='w3-input w3-border w3-round w3-margin-top search w3-mobile' />
             </form>
 
             {
@@ -63,7 +63,7 @@ export default class App extends Component {
               this.state.posts.map((arr,ind)=>{
                 return(
                   <div key={ind}>
-                    <div className='w3-row w3-card w3-round w3-margin-top w3-hover-blue' onClick={()=>{cu.more(`${ind}C`)}} style={{cursor: 'pointer'}}>
+                    <div className='w3-row w3-card w3-round w3-margin-top w3-hover-blue  w3-mobile' onClick={()=>{cu.more(`${ind}C`)}} style={{cursor: 'pointer'}}>
                       <div className='w3-col m6 l6 s6'><p>{arr.post}</p></div>
                       <div className='w3-col m6 l6 s6'><p>{arr.time}</p></div>
                     </div>

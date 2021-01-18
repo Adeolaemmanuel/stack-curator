978777e035adef472f88c28ab8c385238b7387ae
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-18 12:41:08 +0100

    first commit

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
-import {  hm } from '../functions'
+import {  cu,hm } from '../functions'
 import { db } from "../database"
 import { Cookies } from 'react-cookie'
 export default class App extends Component {
@@ -14,26 +14,28 @@ export default class App extends Component {
   
   cookie = new Cookies();
   componentDidMount(){
-    db.collection('Posts').doc(this.cookie.get('id')).onSnapshot(t=>{
-      if(t.exists){
-          this.setState({posts: [...t.data().posts]})
-      }
-    })
+    if(this.cookie.get('id') === ''){
+
+    }else{
+      db.collection('Posts').doc(this.cookie.get('id')).onSnapshot(t=>{
+        if(t.exists){
+            this.setState({posts: [...t.data().posts]})
+        }
+      })
+    }
   }
 
 
   render() {
     return (
       <div className='w3-center'>
-        <div style={{display: 'inline-block'}}>
-          <form >
-            <input type='search' placeholder='Search' id='search' className='w3-input w3-border w3-round w3-margin-top search' />
-          </form>
-        </div>
 
         <div className='w3-row w3-margin-top'>
           <div className='w3-col m3 l3 w3-hide-small'><br /></div>
           <div className='w3-col s12 m6 l6'>
+            <form className=''>
+              <input type='search' placeholder='Search' id='search' className='w3-input w3-border w3-round w3-margin-top search' />
+            </form>
             {
               hm.post()
             }
@@ -42,14 +44,14 @@ export default class App extends Component {
               this.state.posts.map((arr,ind)=>{
                 return(
                   <div key={ind}>
-                    <div className='w3-row w3-card w3-round w3-margin-top'>
+                    <div className='w3-row w3-card w3-round w3-margin-top w3-hover-blue' onClick={()=>{cu.more(`${ind}C`)}} style={{cursor: 'pointer'}}>
                       <div className='w3-col m6 l6 s6'><p>{arr.post}</p></div>
                       <div className='w3-col m6 l6 s6'><p>{arr.time}</p></div>
                     </div>
 
-                    <div>
-                      <div id='comment' className=''>
-                        <form className='w3-container w3-padding' style={{display: 'inline-block'}}>
+                    <div className='w3-hide' id={`${ind}C`}>
+                      <div id='comment'>
+                        <form className='w3-container w3-padding'>
                           <input className='w3-input w3-border w3-round-large' placeholder='Your Opinoin' />
                         </form>
                       </div>
--- src/functions.js
+++ src/functions.js
@@ -104,7 +104,7 @@ class Home extends Functions {
                         <button className='w3-btn w3-black w3-round w3-margin-top' onClick={e=>{cu.postQuestion(e,'curate')}}>Curate</button>
                     </div>
                     <div className='w3-padding w3-card-4 w3-round w3-margin-top w3-margin-bottom w3-hide' id='post'>
-                        <span className='w3-button w3-padding w3-right' onClick={()=>{document.getElementById('post').classList.add('w3-hide')}} >X</span>
+                        <span className='w3-button w3-padding w3-right' onClick={()=>{document.getElementById('post').classList.add('w3-hide');document.getElementById('curate').classList.remove('w3-hide')}} >X</span>
                         <form className='w3-margin-top'>
                             <input className='w3-input w3-border w3-round' type='text' placeholder="Tags" id='tags' />
                             <textarea className='w3-input w3-border w3-round w3-margin-top' id='posts' placeholder="What's on your mind..."></textarea>
@@ -160,6 +160,15 @@ class Curate extends Functions {
                     }
                 })
             }
+        }
+    }
+
+    more = (pram) => {
+        let check = document.getElementById(pram).classList.contains('w3-hide')
+        if(check === true){
+            document.getElementById(pram).classList.remove('w3-hide')
+        }else if(check === false){
+            document.getElementById(pram).classList.add('w3-hide')
         }
     }
 }

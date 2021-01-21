d0e914783a8c283095819c3acd3ccfb7d9c5aa9a
Adeolaemmanuel <adeolaemmanuel006@gmail.com>
2021-01-21 02:04:09 +0100

    modified

--- src/App/nav.js
+++ src/App/nav.js
@@ -1,4 +1,4 @@
-﻿import React, { Component } from 'react'
+import React, { Component } from 'react'
 import bar from '../assets/img/menu.svg'
 import barw from '../assets/img/menuw.svg'
 
@@ -35,7 +35,7 @@ export class Nav extends Component {
             document.querySelector('#sidebar').classList.add('w3-hide');
         }
     }
-    
+
 
     render() {
         return (
@@ -45,16 +45,16 @@ export class Nav extends Component {
                         <img src={this.state.menuBar} onClick={e => this.nav(e, 'open')} alt='menu' style={{ width: '40px', height: '40px', color: this.props.themeSettings.color }} />
                     </div>
                     <div className='w3-center'>
-                        <h4 className='w3-bold' style={{ color: this.props.themeSettings.textColor}}>A CURATOR SIGHS</h4>
+                        <h4 className='w3-bold' style={{ color: this.props.themeSettings.textColor }}>A CURATOR SIGHS</h4>
                     </div>
                 </nav>
-                <div className='w3-sidebar w3-bar-block w3-hide w3-animate-left' id='sidebar' style={{ backgroundColor: this.props.themeSettings.color, position: 'relative', left:'0' }}>
+                <div className='w3-sidebar w3-bar-block w3-hide w3-animate-left' id='sidebar' style={{ backgroundColor: this.props.themeSettings.color, position: 'relative', left: '0' }}>
                     <div className='w3-row w3-bar-item w3-block w3-margin-top'>
                         <div className='w3-col s6 m6 l6 w3-padding' style={{ color: this.props.themeSettings.textColor }}>Theme</div>
                         <div className='w3-col s6 m6 l6'><button className='w3-btn w3-round' style={{ backgroundColor: this.props.themeSettings.textColor, color: this.props.themeSettings.color }} onClick={this.props.theme}>{this.props.themeSettings.name}</button></div>
                     </div>
                 </div>
             </div>
-            )
+        )
     }
 }
